38bb09161c25b00856021edc9da6ba6ece44cc3e
Roman Zahid <romanzahid64@gmail.com>
2026-05-18 16:22:42 +0500

    Add files via upload

---
+++ LOOP/index.js
@@ -0,0 +1,40 @@
+// // Functions & Arrow Functions
+
+// // function declaration
+// function add(a,b) {
+//     return a+b;
+// }
+// console.log(10+11)
+
+// // Function
+// function multiply(num1, num2){
+//     return num1 * num2;
+// }
+// // Chalany ka tareeqa (Usage):
+// console.log(multiply(4,5));
+
+// calculateDiscount
+// function calculateDiscount(totalPrice, discount) {
+//     return totalPrice - discount;
+// }
+
+// // Chalany ka tareeqa (Usage):
+// console.log(calculateDiscount(1000, 200));
+
+// function sayHello(name) {
+//     return "Hello " + name +"! How are you?";
+// }
+
+// // Chalany ka tareeqa (Usage):
+// console.log(sayHello("Niaz"));
+
+// function expression
+// const subtract = function(a,b) {
+//     return a - b;
+// };
+// console.log(subtract(4,5));
+
+ const sayHello = function(name) {
+    return "Hello " + name +"! How are you?";
+ };
+ console.log(sayHello("Sabahat"));
